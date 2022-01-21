a4caa183dc3f3ebb25f7ea65f5a4f9b7a9441f42
Patryk Zawadzki <patryk.zawadzki@intive.com>
2022-01-21 14:00:40 +0100

    Initial commit of project.

---
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
@@ -0,0 +1,13 @@
+package com.intive.patronage22.szczecin.retroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intive.patronage22.szczecin.retroboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
