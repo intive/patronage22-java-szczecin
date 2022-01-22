0b7aa6e0d4bb42cc7738d9a65d895db4c446cdc3
Szachista <jacek.szachista@gmail.com>
2022-01-22 22:10:59 +0100

    FIX upgrade to swagger3

--- src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
@@ -3,10 +3,8 @@ package com.intive.patronage22.szczecin.retroboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableSwagger2
 public class RetroboardApplication {
 
 	public static void main(String[] args) {
