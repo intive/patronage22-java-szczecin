2eefea67da2035e79f750ebeae5e60ff4d817871
Szachista <jacek.szachista@gmail.com>
2022-01-22 15:59:36 +0100

    Add swagger dependency

--- src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
@@ -2,8 +2,11 @@ package com.intive.patronage22.szczecin.retroboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableSwagger2
 public class RetroboardApplication {
 
 	public static void main(String[] args) {
