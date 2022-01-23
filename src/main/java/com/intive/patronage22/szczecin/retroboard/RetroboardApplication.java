2b10c054a56f0c95cbee09f6e3ea40fa71a0ea33
Szachista <jacek.szachista@gmail.com>
2022-01-23 15:59:58 +0100

    FIX Remove exclusion of DataSourceAutoConfiguration

--- src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
@@ -2,9 +2,8 @@ package com.intive.patronage22.szczecin.retroboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class RetroboardApplication {
 
 	public static void main(String[] args) {
