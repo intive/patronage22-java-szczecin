950c71377bcc82f5c8ed2e692780fd0e565a1c88
macie <maciejstanulewicz2004@wp.pl>
2022-02-02 19:31:22 +0100

    Edit checkstyle.xml

--- src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/RetroboardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RetroboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RetroboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RetroboardApplication.class, args);
+    }
 }
