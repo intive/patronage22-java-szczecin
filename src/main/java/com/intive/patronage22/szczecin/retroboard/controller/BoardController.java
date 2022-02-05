29ff85c4aa8949c20aef57c1b036c32e123bddc7
macie <maciejstanulewicz2004@wp.pl>
2022-02-05 19:02:12 +0100

    Creat POST method

--- src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardController.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardController.java
@@ -2,13 +2,10 @@ package com.intive.patronage22.szczecin.retroboard.controller;
 
 import com.intive.patronage22.szczecin.retroboard.dto.BoardDto;
 import com.intive.patronage22.szczecin.retroboard.service.BoardService;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,14 +19,9 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "400", description = "Bad request data"),
-            @ApiResponse(responseCode = "404", description = "User not found")
-    })
-    public ResponseEntity<List<BoardDto>> getUserBoards(
-            @RequestParam(name = "userId") final String uid) {
-        List<BoardDto> boards = boardService.getUserBoards(uid);
+    public ResponseEntity<List<BoardDto>> getBoards() {
+        final List<BoardDto> boards = boardService.mockBoardData();
+
         return ResponseEntity.status(OK).body(boards);
     }
 }
