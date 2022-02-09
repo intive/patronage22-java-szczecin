c5223fb6601648feb939343d48406a28d7d3148b
Aleksandra Karasińska <45500262+golldfish@users.noreply.github.com>
2022-02-09 14:56:05 +0100

    P2022-449 Firebase initial implementation. (#18)
    * Created configuration for Firebase
    
    * Changed line lenght to 120 in checkstyle.xml, fixed checkstyle mistakes
    
    * Simplified method
    
    * Fixed bug with docker
    
    * Added sample .env file, updated README.md
    
    * Delete .env
    
    * Removed data from env, changed readme, added override in tests
    
    * Fixed Bean in tests

--- README.md
+++ README.md
@@ -12,3 +12,30 @@ docker run -p 8080:8080 --rm -it retroboard:latest
 ```
 http://localhost:8080/swagger-ui/index.html
 ```
+
+### Firebase
+#### Creating json file with needed values
+To generate a private key file: 
+- Go to: [a link](https://console.firebase.google.com/u/0/project/_/settings/serviceaccounts/adminsdk)
+- Create new project or use exiting one 
+- In the Firebase console, open Settings > Service Accounts
+- Click Generate New Private Key, then confirm by clicking Generate Key
+
+#### Adding values to local variables
+To be able to run Firebase everyone needs to set variables in .env file.
+
+To add values:
+- Open private key json downloaded in previous step
+- Change values to yours in .env file
+
+Following values need to be set:
+- FIREBASE_CLIENT_ID=**your_client_id**
+- FIREBASE_CLIENT_EMAIL=**your_client_email**
+- FIREBASE_CLIENT_PRIVATE_KEY_ID=**your_private_key_id** 
+- FIREBASE_CLIENT_PRIVATE_KEY_PKCS8=**your_private_key** 
+- FIREBASE_PROJECT_ID=**your_project_id**
+
+#### Run Docker with .env file
+```bash
+docker-compose up --build
+```
---
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/configuration/FirebaseConfiguration.java
@@ -0,0 +1,37 @@
+package com.intive.patronage22.szczecin.retroboard.configuration;
+
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfiguration {
+
+    @Bean
+    public FirebaseApp firebaseApp(@Value("${FIREBASE_CLIENT_ID}") final String clientId,
+                                   @Value("${FIREBASE_CLIENT_EMAIL}") final String clientEmail,
+                                   @Value("${FIREBASE_CLIENT_PRIVATE_KEY_ID}") final String clientPrivateKeyId,
+                                   @Value("${FIREBASE_CLIENT_PRIVATE_KEY_PKCS8}") final String clientPrivateKeyPkcs8,
+                                   @Value("${FIREBASE_PROJECT_ID}") final String projectId) throws IOException {
+        final ServiceAccountCredentials serviceAccountCredentials =
+                ServiceAccountCredentials.fromPkcs8(clientId, clientEmail, clientPrivateKeyPkcs8.replace("\\n", "\n"),
+                        clientPrivateKeyId, null);
+        final FirebaseOptions options =
+                FirebaseOptions.builder().setProjectId(projectId).setCredentials(serviceAccountCredentials).build();
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseAuth firebaseAuth(final FirebaseApp firebaseApp) {
+        return FirebaseAuth.getInstance(firebaseApp);
+    }
+
+}
+
+
