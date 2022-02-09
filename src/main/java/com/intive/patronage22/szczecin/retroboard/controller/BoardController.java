ada73d81eaa84196b006442aedb580b3ec22a87b
Szachista <jacek.szachista@gmail.com>
2022-02-09 12:36:21 +0100

    Add ApiResponse annotation

--- src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardController.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.intive.patronage22.szczecin.retroboard.controller;
 
 import com.intive.patronage22.szczecin.retroboard.dto.BoardDto;
 import com.intive.patronage22.szczecin.retroboard.service.BoardService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,11 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad request data"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<List<BoardDto>> getUserBoards(
             @RequestParam(name = "userId", required = false) String uid) {
         List<BoardDto> boards = boardService.getUserBoards(uid);
