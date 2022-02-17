2f48b6a8daf9c302c86071b37738a5b99d280d21
edych <edytapatrycja.chrzanowska@gmail.com>
2022-02-17 18:06:37 +0100

    P2022-1014, added user registration with firebase

--- src/test/java/com/intive/patronage22/szczecin/retroboard/service/UserServiceTest.java
+++ src/test/java/com/intive/patronage22/szczecin/retroboard/service/UserServiceTest.java
@@ -3,6 +3,10 @@ package com.intive.patronage22.szczecin.retroboard.service;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
+import com.intive.patronage22.szczecin.retroboard.exception.DisplayNameFormatException;
+import com.intive.patronage22.szczecin.retroboard.exception.EmailFormatException;
+import com.intive.patronage22.szczecin.retroboard.exception.PasswordFormatException;
+import com.intive.patronage22.szczecin.retroboard.exception.UserAlreadyExistException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +16,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -36,22 +42,76 @@ public class UserServiceTest {
         final String displayName = "someuser";
         final String encodedPassword = "$2a$10$svvZN3tiBna47JHfN8AptuOSqbzUlLfAuY9ollfC1J3.CcC2yVH6y";
 
-        final UserRecord.CreateRequest request = new UserRecord.CreateRequest()
-                .setEmail(email)
-                .setEmailVerified(true)
-                .setDisplayName(displayName)
-                .setPassword(password);
+        final UserRecord userRecord = mock(UserRecord.class);
 
         // when
         when(firebaseAuth.getUserByEmail(email)).thenThrow(FirebaseAuthException.class);
-
-        // TODO cannot create new UserRecord instance
-        //when(firebaseAuth.createUser(request)).thenReturn(new UserRecord());
+        when(userRecord.getEmail()).thenReturn(email);
+        when(firebaseAuth.createUser(any())).thenReturn(userRecord);
         when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
 
         // then
         final UserDetails returnedUser = userService.register(email, password, displayName);
 
         assertNull(returnedUser.getPassword());
+        assertEquals(returnedUser.getUsername(), email);
+    }
+
+    @Test
+    public void shouldThrowUserAlreadyExistsExceptionWhenEmailIsTaken() throws FirebaseAuthException {
+        // given
+        final String email = "test22@test.com";
+        final String password = "123456";
+        final String displayName = "someuser";
+
+        final UserRecord userRecord = mock(UserRecord.class);
+
+        // when
+        when(firebaseAuth.getUserByEmail(email)).thenReturn(userRecord);
+
+        // then
+        assertThrows(UserAlreadyExistException.class, () -> userService.register(email, password, displayName));
+    }
+
+    @Test
+    public void shouldThrowEmailFormatExceptionWhenEmailIsNotValid() throws FirebaseAuthException {
+        // given
+        final String email = "test22@.com";
+        final String password = "123456";
+        final String displayName = "someuser";
+
+        // when
+        when(firebaseAuth.getUserByEmail(email)).thenThrow(FirebaseAuthException.class);
+
+        // then
+        assertThrows(EmailFormatException.class, () -> userService.register(email, password, displayName));
+    }
+
+    @Test
+    public void shouldThrowPasswordFormatExceptionWhenPasswordIsNotValid() throws FirebaseAuthException {
+        // given
+        final String email = "test22@test.com";
+        final String password = "12";
+        final String displayName = "someuser";
+
+        // when
+        when(firebaseAuth.getUserByEmail(email)).thenThrow(FirebaseAuthException.class);
+
+        // then
+        assertThrows(PasswordFormatException.class, () -> userService.register(email, password, displayName));
+    }
+
+    @Test
+    public void shouldThrowDisplayNameFormatExceptionWhenDisplayNameIsNotValid() throws FirebaseAuthException {
+        // given
+        final String email = "test22@test.com";
+        final String password = "123456";
+        final String displayName = "";
+
+        // when
+        when(firebaseAuth.getUserByEmail(email)).thenThrow(FirebaseAuthException.class);
+
+        // then
+        assertThrows(DisplayNameFormatException.class, () -> userService.register(email, password, displayName));
     }
 }
