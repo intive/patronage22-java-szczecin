f90e94327bd27c4b825c34f9ac0ea9dc31e6f3a1
Szachista <jacek.szachista@gmail.com>
2022-02-19 17:09:49 +0100

    Resolve issues

--- src/main/java/com/intive/patronage22/szczecin/retroboard/configuration/security/SecurityConfig.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/configuration/security/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.intive.patronage22.szczecin.retroboard.configuration.security;
 import com.intive.patronage22.szczecin.retroboard.provider.FirebaseAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -17,10 +15,9 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Primary
     @Bean
-    AuthenticationProvider authenticationProvider(final FirebaseAuthenticationProvider firebaseAuthenticationProvider) {
-        return firebaseAuthenticationProvider;
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
