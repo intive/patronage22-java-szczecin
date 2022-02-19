17e32aa6333b25933ac0d5a65fd0e584bfc58f5a
Szachista <jacek.szachista@gmail.com>
2022-02-19 08:17:08 +0100

    Fix test failure

--- src/main/java/com/intive/patronage22/szczecin/retroboard/configuration/security/SecurityConfig.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/configuration/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.intive.patronage22.szczecin.retroboard.configuration.security;
 import com.intive.patronage22.szczecin.retroboard.provider.FirebaseAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,12 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Primary
+    @Bean
+    AuthenticationProvider authenticationProvider(final FirebaseAuthenticationProvider firebaseAuthenticationProvider) {
+        return firebaseAuthenticationProvider;
     }
 
     @Bean
