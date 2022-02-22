df1a0086bf3b8aa44d411041204c8f66a912fe50
Patryk Zawadzki <95465602+patrykzawadzkiintive@users.noreply.github.com>
2022-02-22 09:38:06 +0100

    Swagger authorization button with authorization token functionality (#29)
    Add authorize button and authorization header functionality for given endpoints.

--- src/main/java/com/intive/patronage22/szczecin/retroboard/controller/UserController.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/controller/UserController.java
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.intive.patronage22.szczecin.retroboard.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -21,6 +22,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/private")
+    @Operation(security = @SecurityRequirement(name = "tokenAuth"))
     Authentication privateOp() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
@@ -35,5 +37,17 @@ public class UserController {
                          @RequestParam final String displayName) throws FirebaseAuthException {
 
         return userService.register(email, password, displayName);
+    }
+
+    @PostMapping(value = "/login", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    @ResponseStatus(CREATED)
+    @Operation(summary = "Login in user using provided email and password.",
+            responses = {@ApiResponse(responseCode = "200", description = "Successfully logged in."),
+                    @ApiResponse(responseCode = "400", description = "Email or password is not valid")})
+    public void fakeLogin(@RequestParam("email") final String email, @RequestParam("password") final String password) {
+        throw new IllegalStateException(
+                "This method shouldn't be called. It's implemented by Spring Security filters. " +
+                "The reason for it's creation is to override bug in /login swagger documentation " +
+                        "https://github.com/springdoc/springdoc-openapi/issues/827 ");
     }
 }
