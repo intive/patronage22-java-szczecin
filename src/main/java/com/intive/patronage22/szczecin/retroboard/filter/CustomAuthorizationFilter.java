f2eb89c4228fec1d995ba3289ec5be9b5b230560
macie <maciejstanulewicz2004@wp.pl>
2022-02-28 22:32:34 +0100

    fix

--- src/main/java/com/intive/patronage22/szczecin/retroboard/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/filter/CustomAuthorizationFilter.java
@@ -5,6 +5,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
