a62341e13d4ca997dccc08da23ff6aefe030592d
golldfish <a.karasinska13@gmail.com>
2022-03-17 16:35:32 +0100

    Added functionality for voting

--- src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardCardController.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/controller/BoardCardController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import java.util.Map;
+
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -43,7 +45,7 @@ public class BoardCardController {
 
         return boardCardService.createBoardCard(boardCardDto, boardId, authentication.getName());
     }
-    
+
     @DeleteMapping("/{id}")
     @ResponseStatus(OK)
     @Operation(security = @SecurityRequirement(name = "tokenAuth"), summary = "Remove card from the board.",
@@ -54,5 +56,18 @@ public class BoardCardController {
                                        final Authentication authentication) {
 
         boardCardService.removeCard(cardId, authentication.getName());
+    }
+
+    @PostMapping("/{id}/votes")
+    @ResponseStatus(CREATED)
+    @Operation(security = @SecurityRequirement(name = "tokenAuth"), summary = "Add card to the board.",
+               responses = {@ApiResponse(responseCode = "201", description = "Voted"),
+                       @ApiResponse(responseCode = "400",
+                                    description = "No more votes. Board state is not in state VOTING"),
+                       @ApiResponse(responseCode = "404", description = "Board/Card not found")})
+    public Map<String, Integer> vote(@PathVariable(name = "id") final Integer cardId,
+                                     final Authentication authentication) {
+
+        return boardCardService.vote(cardId, authentication.getName());
     }
 }
--- src/main/java/com/intive/patronage22/szczecin/retroboard/repository/BoardCardsVotesRepository.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/repository/BoardCardsVotesRepository.java
@@ -1,10 +1,25 @@
 package com.intive.patronage22.szczecin.retroboard.repository;
 
+import com.intive.patronage22.szczecin.retroboard.model.Board;
+import com.intive.patronage22.szczecin.retroboard.model.BoardCard;
 import com.intive.patronage22.szczecin.retroboard.model.BoardCardVotes;
 import com.intive.patronage22.szczecin.retroboard.model.BoardCardVotesKey;
+import com.intive.patronage22.szczecin.retroboard.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardCardsVotesRepository extends CrudRepository<BoardCardVotes, BoardCardVotesKey> {
+
+    Optional<BoardCardVotes> findByCardAndVoter(final BoardCard card, final User user);
+
+    @Query(value = "SELECT bcv.votes FROM BoardCardVotes bcv JOIN BoardCard bc ON bcv.card.id = bc.id WHERE bc.board " +
+                   "= :board AND bcv.voter=:user")
+    List<Integer> getCountsByBoardAndUser(@Param("board") final Board board, @Param("user") final User user);
+
 }
