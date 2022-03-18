9bca65362d0a65960b79c823fe439d77d273e94d
Szachista <jacek.szachista@gmail.com>
2022-03-18 08:54:51 +0100

    Use of equals method

--- src/main/java/com/intive/patronage22/szczecin/retroboard/service/BoardCardService.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/service/BoardCardService.java
@@ -64,8 +64,8 @@ public class BoardCardService {
         final User user = userRepository
                 .findUserByEmail(email).orElseThrow(() -> new BadRequestException("User not found"));
 
-        if (!email.equals(boardCard.getCreator().getEmail())
-                && !email.equals(boardCard.getBoard().getCreator().getEmail()))
+        if (!boardCard.getCreator().equals(user)
+                && !boardCard.getBoard().getCreator().equals(user))
             throw new BadRequestException("User is not allowed to delete card");
 
         if (!EnumStateDto.CREATED.equals(boardCard.getBoard().getState()))
