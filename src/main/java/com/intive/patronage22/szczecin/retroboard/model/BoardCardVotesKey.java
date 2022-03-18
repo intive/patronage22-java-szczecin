9831c9cbabeabdeae4d60e496a72d7e4aff9094f
Aleksandra Karasińska <45500262+golldfish@users.noreply.github.com>
2022-03-18 15:57:17 +0100

    P2022-1512 - Voting functionality - vote (#63)
    * Added functionality for voting
    
    * Added tests to BoardCardServiceTest class
    
    * Added tests to BoardCardServiceTest class
    
    * Added tests to BoardCardController.java
    
    * Fixed checkstyle issue
    
    * Removed empty line
    
    * Added checking exception messages in tests
    
    * Refactor and fixed enum checking
    
    * P2022-1789 Refactor tests removeUserAssignedToTheBoard (#64)
    
    * P2022-1789 Refactor tests removeUserAssignedToTheBoard
    
    * quick refactor
    
    * quick refactor
    
    * quick refactor
    
    * Add 'remove board card' functionality (#58)
    
    * Add 'remove board card' functionality
    
    * Fix exception messages
    
    * Add build methods, correct 'removeBoardCardShouldThrowNotFoundExceptionWhenBoardCardIsNotFound' test
    
    * Add 'remove board card' functionality
    
    * Fix exception messages
    
    * Add build methods, correct 'removeBoardCardShouldThrowNotFoundExceptionWhenBoardCardIsNotFound' test
    
    * Fix
    
    * Remove empty lines
    
    * Add 'remove board card' functionality
    
    * Fix exception messages
    
    * Add build methods, correct 'removeBoardCardShouldThrowNotFoundExceptionWhenBoardCardIsNotFound' test
    
    * Fix
    
    * Add build methods, correct 'removeBoardCardShouldThrowNotFoundExceptionWhenBoardCardIsNotFound' test
    
    * Remove empty lines
    
    * Use of equals method
    
    * Fix equals
    
    * Added functionality for voting
    
    * Added tests to BoardCardServiceTest class
    
    * Added tests to BoardCardServiceTest class
    
    * Added tests to BoardCardController.java
    
    * Fixed checkstyle issue
    
    * Removed empty line
    
    * Added checking exception messages in tests
    
    * Refactor and fixed enum checking
    
    * Fixed test
    
    Co-authored-by: Edyta Chrzanowska <43759530+edych@users.noreply.github.com>
    Co-authored-by: Jacek Klimaszewski <Szachista@users.noreply.github.com>

--- src/main/java/com/intive/patronage22/szczecin/retroboard/model/BoardCardVotesKey.java
+++ src/main/java/com/intive/patronage22/szczecin/retroboard/model/BoardCardVotesKey.java
@@ -1,5 +1,6 @@
 package com.intive.patronage22.szczecin.retroboard.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.io.Serializable;
 @Data
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 public class BoardCardVotesKey implements Serializable {
 
     @Column(name = "card_id", nullable = false)
